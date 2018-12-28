5de49e11d725f16727d45fd1b583ed4b0bcde305
André Bauer <andre.bauer@kiwigrid.com>
2018-12-29 00:45:22 +0100

    changed ci to reflect new directory structure

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -8,28 +8,33 @@ set -ex
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 CHART_SOURCE="https://github.com/zammad/helm.git"
 CHART_REPO="git@github.com:zammad/zammad.github.io.git"
-DIR_NAME="zammad"
+DIR_NAME="chart"
 
 # remove zammad dir if exist
 test -d ${REPO_ROOT}/${DIR_NAME} && rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}
 
 # get chart source
 git clone ${CHART_SOURCE} ${DIR_NAME}
-rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}/.git
+rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}/zammad/.git
 
 # get chart version
-CHART_VERSION="$(grep version: ${REPO_ROOT}/${DIR_NAME}/Chart.yaml | sed 's/version: //')"
+CHART_VERSION="$(grep version: ${REPO_ROOT}/${DIR_NAME}/zammad/Chart.yaml | sed 's/version: //')"
 
 # build helm dependencies
-cd ${DIR_NAME}
+cd ${DIR_NAME}/zammad
 helm dependency build
 cd ..
 
 # build chart
-helm package ${DIR_NAME}
+helm package zammad
+
+test -f ${REPO_ROOT}/index.yaml && cp ${REPO_ROOT}/index.yaml ${REPO_ROOT}/${DIR_NAME}/index.yaml
 
 # create repo index
 helm repo index --merge index.yaml --url https://zammad.github.io .
+
+cp ${REPO_ROOT}/${DIR_NAME}/index.yaml ${REPO_ROOT}/index.yaml
+cp ${REPO_ROOT}/${DIR_NAME}/*.tgz ${REPO_ROOT}
 
 # push changes to github
 if [ "${TRAVIS}" == 'true' ]; then
