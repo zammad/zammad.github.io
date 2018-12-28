e48c93745dddaf439cfac60bd3e85f952ec7b5ba
André Bauer <andre.bauer@kiwigrid.com>
2018-12-28 16:18:40 +0100

    added helm install to travis

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -10,21 +10,28 @@ CHART_SOURCE="https://github.com/zammad/helm.git"
 CHART_REPO="https://zammad.github.io"
 DIR_NAME="zammad"
 
+# remove zammad dir fi exist
 test -d ${REPO_ROOT}/${DIR_NAME} && rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}
 
+# get chart source
 git clone ${CHART_SOURCE} ${DIR_NAME}
 rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}/.git
 
+# get chart version
 CHART_VERSION="$(grep version: ${REPO_ROOT}/${DIR_NAME}/Chart.yaml | sed 's/version: //')"
 
+# build helm dependencies
 cd ${DIR_NAME}
 helm dependency build
 cd ..
 
+# build chart
 helm package ${DIR_NAME}
 
+# build repo index
 helm repo index --merge index.yaml --url https://zammad.github.io .
 
+# git push
 #git remote add
 # git add --all .
 # git commit -m "added zammad helm chart with version ${CHART_VERSION}"
