be6de389b5614dcbe09142cc42a8130d5b2e09ef
André Bauer <andre.bauer@kiwigrid.com>
2018-12-29 16:23:46 +0100

    fixed shellcheck errors

--- .circleci/ci.sh
+++ .circleci/ci.sh
@@ -20,38 +20,40 @@ helm repo update
 
 
 # remove zammad dir if exist
-test -d ${REPO_ROOT}/${DIR_NAME} && rm -rf ${REPO_ROOT:=?}/${DIR_NAME:=?}
+test -d "${REPO_ROOT}/${DIR_NAME}" && rm -r "${REPO_ROOT:=?}/${DIR_NAME:=?}"
 
 # get chart source
-git clone ${CHART_SOURCE} ${DIR_NAME}
+git clone "${CHART_SOURCE}" "${DIR_NAME}"
 
-rm -r ${REPO_ROOT}/${DIR_NAME}/zammad/.git
+rm -r "${REPO_ROOT}"/"${DIR_NAME}"/zammad/.git
 
 # get chart version
-CHART_VERSION="$(grep version: ${REPO_ROOT}/${DIR_NAME}/zammad/Chart.yaml | sed 's/version: //')"
+CHART_VERSION="$(grep version: "${REPO_ROOT}"/"${DIR_NAME}"/zammad/Chart.yaml | sed 's/version: //')"
 
 # build helm dependencies
-cd ${DIR_NAME}/zammad
+(
+cd "${DIR_NAME}"/zammad || exit
 helm dependency build
-cd ..
+)
+
 
 # build chart
 helm package zammad
 
-test -f ${REPO_ROOT}/index.yaml && cp ${REPO_ROOT}/index.yaml ${REPO_ROOT}/${DIR_NAME}/index.yaml
+test -f "${REPO_ROOT}"/index.yaml && cp "${REPO_ROOT}"/index.yaml "${REPO_ROOT}"/"${DIR_NAME}"/index.yaml
 
 # create repo index
 helm repo index --merge index.yaml --url https://zammad.github.io .
 
-cp ${REPO_ROOT}/${DIR_NAME}/index.yaml ${REPO_ROOT}/index.yaml
-cp ${REPO_ROOT}/${DIR_NAME}/*.tgz ${REPO_ROOT}
+cp "${REPO_ROOT}"/"${DIR_NAME}"/index.yaml "${REPO_ROOT}"/index.yaml
+cp "${REPO_ROOT}"/"${DIR_NAME}"/*.tgz "${REPO_ROOT}"
 
 # push changes to github
 if [ "${TRAVIS}" == 'true' ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git remote remove origin
-  git remote add origin ${CHART_REPO}
+  git remote add origin "${CHART_REPO}"
   git checkout master
   git add --all .
   git commit -m "push zammad chart version ${CHART_VERSION} via travis build nr: ${TRAVIS_BUILD_NUMBER} - [skip travis-ci]"
